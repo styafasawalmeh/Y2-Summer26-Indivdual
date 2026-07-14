e2786dd29a165620a37e3974523eb89ccd4fa1a0
Yafas27-meet <st.yafa.sawalmeh@rosaryshs-j.com>
2026-07-14 22:37:43 +0300

    Lab2

--- agent/app.py
+++ agent/app.py
@@ -11,12 +11,13 @@ def run_chat():
 
     system_message = (
         "Your name is Yafa. You are a helpful and friendly assistant "
-        "who helps students learn about technology and computer science."
+        "who helps students learn about anything they ask you to. you are annoying, and just ragebait people"
     )
 
 
+    history = []
+
     while True:
-        history = []
 
         user_input = input(">> ")
 
@@ -24,21 +25,33 @@ def run_chat():
             break
 
         history.append({"role": "user", "content": user_input})
+        print('History:', history)
+  # Add this BEFORE the API call to inspect the full response:
+# print(response)
 
-        response = client.messages.create(
-            model="claude-haiku-4-5-20251001",
-            max_tokens=300,
-            temperature=0.7,
-            
-            system=system_message,
-            messages=history,
-        )
+# Add this BEFORE the API call to see the full message history:
+# print('History so far:', history)
 
-        reply = response.content[0].text
-        print(f"Claude: {reply}")
+# Parameters you will experiment with:
+response = client.messages.create(
+    model='claude-haiku-4-5-20251001',
+    max_tokens=300,   # try 50, then 500
+    temperature=0.7,  # try 0, then 1
+    system=system_message,
+    messages=history
+)      
+print(response)
+reply = response.content[0].text
+print(f"Claude: {reply}")
 
-        history.append({"role": "assistant", "content": reply})
+history.append({"role": "assistant", "content": reply})
 
 run_chat()
 
-#step 2
+#step 1
+#usage.input_tokens: the text we input yo the AI
+#sage.output_tokens: the response we get back
+#step 3:
+#why does API need the full history very single time?
+#to process the history 
+
