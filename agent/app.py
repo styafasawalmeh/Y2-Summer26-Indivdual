e5bc3678c7e3dc772873d6bcb57128e0e12357b4
Yafas27-meet <st.yafa.sawalmeh@rosaryshs-j.com>
2026-07-15 15:46:36 +0300

    lab2.july15th.yafa

--- agent/app.py
+++ agent/app.py
@@ -10,8 +10,8 @@ def run_chat():
     print("You: (type exit to quit)")
 
     system_message = (
-        "Your name is Yafa. You are a helpful and friendly assistant "
-        "who helps students learn about anything they ask you to. you are annoying, and just ragebait people"
+        "Your name is Hiba. You are a helpful and friendly assistant "
+        "who helps students learn about anything they ask you to. be friendly, sweet and caring! you're like their best friend"
     )
 
 
@@ -25,7 +25,7 @@ def run_chat():
             break
 
         history.append({"role": "user", "content": user_input})
-        print('History:', history)
+        #print('History:', history)
   # Add this BEFORE the API call to inspect the full response:
 # print(response)
 
@@ -33,25 +33,30 @@ def run_chat():
 # print('History so far:', history)
 
 # Parameters you will experiment with:
-response = client.messages.create(
-    model='claude-haiku-4-5-20251001',
-    max_tokens=300,   # try 50, then 500
-    temperature=0.7,  # try 0, then 1
-    system=system_message,
-    messages=history
-)      
-print(response)
-reply = response.content[0].text
-print(f"Claude: {reply}")
+        response = client.messages.create(
+        model='claude-haiku-4-5-20251001',
+        max_tokens=300,   # try 50, then 500
+        temperature=0.7,  # try 0, then 1
+        system=system_message,
+        messages=history
+    )      
+        print(response)
+        reply = response.content[0].text
+        print(f"Claude: {reply}")
 
-history.append({"role": "assistant", "content": reply})
-
+    history.append({"role": "assistant", "content": reply})
 run_chat()
 
-#step 1
-#usage.input_tokens: the text we input yo the AI
-#sage.output_tokens: the response we get back
-#step 3:
-#why does API need the full history very single time?
-#to process the history 
+    #step 1
+    #usage.input_tokens: the text we input yo the AI
+    #sage.output_tokens: the response we get back
+    #step 3:
+    #why does API need the full history very single time?
+    #to process the history 
+
+    #reflection_ lab2;
+    #1. maybe when you go to a store that is very cheap so you buy this and this and that but the end price is a lot even if individual items are cheap
+    #2.prediction, API doesn't work: the history won't hsve the uder's input 
+    #3. i encoutred a namerror, an error with the hisroty and an indendation error, 
+    #3. the fixes for them were pretty straightfowrward
 
